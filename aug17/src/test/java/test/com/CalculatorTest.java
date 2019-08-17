1f74cff1fec9719ba8032c425dfd7d2820f3f44a
tspatel02 <tspatel02@yahoo.com>
2019-08-17 13:56:09 -0400

    initial push

---
+++ aug17/src/test/java/test/com/CalculatorTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.itexps.Calculator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Owner
+ */
+public class CalculatorTest {
+    Calculator cal;
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+      cal = new Calculator();
+    
+    }
+    
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testAdd() {
+         assertEquals(4, cal.add(2,2));
+     }
+     
+      @Test
+     public void testSub() {
+         assertEquals(4, cal.sub(6,2));
+     }
+     
+}
---
+++ aug17/src/test/java/test/com/CustomerTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.itexps.Customer;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Owner
+ */
+public class CustomerTest {
+    Customer cust;
+    
+    public CustomerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+        cust = new Customer();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testName() {
+        
+        cust.setName("tom");
+        assertEquals("tom", cust.getName());
+    
+    }
+}
